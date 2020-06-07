513d9b3b6ff083a946fc9bc3ee5096d5a172577a
alolika1998 <66350376+alolika1998@users.noreply.github.com>
2020-06-07 23:17:06 +0530

    Update main.go

--- main.go
+++ main.go
@@ -8,53 +8,54 @@ import (
 	"github.com/gorilla/mux"
 )
 type Person struct {
-	Id int `json:"id"`
-	Firstname string `json:"firstname"`
-	Lastname string `json:"lastname"`
-	Address *Address `json:"address"`  
+	ID string `json:"id,omitempty"`
+	Firstname string `json:"firstname,omitempty"`
+	Lastname string `json:"lastname,omitempty"`
+	Address *Address `json:"address,omitempty"`  
 }
 type Address struct {
-	City string `json:"city"`
-	State string `json:"state"`
+	City string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
 }
 var people []Person
 func GetPersonEndpoint(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	for _, item := ranges people {
-		if item.Id == params["Id"] {
+	for _, item := range people {
+		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
 			return
 		}
 	}
 	json.NewEncoder(w).Encode(&Person{})
 }
-func GetPeopleEndpoint(w http.ResopnseWriter, r *http.Request) {
+func GetPeopleEndpoint(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(people)
 }
 func CreatePersonEndpoint(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	_ = json.NewDecoder(r.Body).Decode(&Person)
-	person.Id = params ["Id"] 
-	people = append(people, Person)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params ["id"] 
+	people = append(people, person)
 	json.NewEncoder(w).Encode(people)
 }
-fund DeletePersonEndpoint(w http.ResponceWriter, r *http.Request) {
+func DeletePersonEndpoint(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for index, item := range people {
-		if item.Id = params["Id"] {
+		if item.ID == params["id"] {
 			people = append(people[:index], people[index+1:]...)
 			break
 		}
 	} 
-	json.NewDecoderencoder(w).Encode(people)
+	json.NewEncoder(w).Encode(people)
 }
 func main() {
 	router := mux.NewRouter()
-	people = append(people, Person{Id: 1, Firstname: "Lisa", Lastname: "Roy", Address: &Address{City: "Kolkata", State: "West Begal"}})
-	people = append(people, Person{Id: 2, Firstname: "Sam", Lastname: "West"})
+	people = append(people, Person{ID: "1", Firstname: "Lisa", Lastname: "Roy", Address: &Address{City: "Kolkata", State: "West Begal"}})
+	people = append(people, Person{ID: "2", Firstname: "Sam", Lastname: "West"})
 	router.HandleFunc("/people", GetPeopleEndpoint).Methods("GET")
-	router.HandleFunc("/people{Id}", GetPersonEndpoint).Methods("GET")
-	router.HandleFunc("/people{Id}", CreatePersonEndpoint).Methods("POST")
-	router.HandleFunc("/people{Id}", DeletePersonEndpoint).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	router.HandleFunc("/people{id}", GetPersonEndpoint).Methods("GET")
+	router.HandleFunc("/people{id}", CreatePersonEndpoint).Methods("POST")
+	router.HandleFunc("/people{id}", DeletePersonEndpoint).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":12345", router))
 }
